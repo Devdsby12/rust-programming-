95b5d3c07f1a435b46264ab1bc83ffabe381ee57
Devdsby12 <144847905+Devdsby12@users.noreply.github.com>
2026-04-04 01:41:16 +0530

    Create BasicDataTypes.rs

---
+++ BasicDataTypes.rs
@@ -0,0 +1,16 @@
+// data types
+fn main(){
+
+    let a : i32 = 99 ;
+    let example : bool = true ;
+    let exampleoffloat : f32  = 66.5 ;
+    let exampleofchar : char = 'a' ;
+
+    let mut mutableExampleOfString : &str = "fahh" ;
+    println!("{}",mutableExampleOfString) ;
+    mutableExampleOfString = "hehe" ;
+
+    println!("{}",mutableExampleOfString) ;
+    
+    const Name : i8 = 9 ; // must type type of variable with const else error
+}
