3256175a39c1a019006c2d9bde2b5729fb2e3aea
Devdsby12 <144847905+Devdsby12@users.noreply.github.com>
2026-04-04 02:42:54 +0530

    Add comment for match statement in main function
    Added comment explaining the match statement usage.

--- match.rs
+++ match.rs
@@ -2,7 +2,7 @@ fn main() {
 	let dev = 55 ;
     let s = 55 ;
     match dev {
-    	77 | 05 => println!("dev") ,
+    	77 | 05 => println!("dev") ,  // can be used for if conditon like x if x == something => then instruction  in end ,
       
         _ => println!("fh") 
         }
